fd06b1d107c5c1708c4cab9de1be17e2d3f4434b
wakky-alcedo <wakky.alcedo@gmail.com>
2024-09-03 16:51:01 +0900

    add: wrapperを追加

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "wrapper.hpp"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -90,7 +90,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-
+  init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -100,6 +100,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  loop();
   }
   /* USER CODE END 3 */
 }
---
+++ Core/Src/wrapper.cpp
@@ -0,0 +1,29 @@
+#include "wrapper.hpp"
+
+/* Pre-Processor Begin */
+
+/* Pre-Processor End */
+
+/* Enum, Struct Begin */
+
+/* Enum, Struct End */
+
+/* Function Prototype Begin */
+
+/* Function Prototype End */
+
+/* Variable Begin */
+
+/* Variable End */
+
+void init(void){
+
+}
+
+void loop(void){
+
+}
+
+/* Function Body Begin */
+
+/* Function Body End */

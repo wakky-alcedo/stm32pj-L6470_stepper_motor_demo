1ffd55aeaf0ffa17e3f52316026f83694a215657
wakky-alcedo <wakky.alcedo@gmail.com>
2024-09-06 19:57:58 +0900

    refactor: 色々消して，もらったプログラムをsubmoduleとして読み込むようにした

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -1,11 +1,9 @@
+#include <l6470.h>
 #include "wrapper.hpp"
-#include <L6470_bigstone.h>
 #include "gpio.h"
 #include "spi.h"
-#include "../../stm32lib-L6470_old/l6470.h"
 
 /* Pre-Processor Begin */
-#define IS_BIGSTONE 0
 /* Pre-Processor End */
 
 /* Enum, Struct Begin */
@@ -17,61 +15,58 @@
 /* Function Prototype End */
 
 /* Variable Begin */
-l6470::L6470 l6470_driver(hspi1,{SPI1_CS_GPIO_Port,SPI1_CS_Pin},{L6470_BUSY_GPIO_Port,L6470_BUSY_Pin},1000);
-l6470_big::L6470 l6470_driver_big(hspi1,{SPI1_CS_GPIO_Port,SPI1_CS_Pin});
+//l6470::L6470 l6470_driver(hspi1,{SPI1_CS_GPIO_Port,SPI1_CS_Pin},{L6470_BUSY_GPIO_Port,L6470_BUSY_Pin},1000);
+l6470::L6470 l6470_driver(hspi1,{SPI1_CS_GPIO_Port,SPI1_CS_Pin});
 /* Variable End */
 
 void init(void){
-#if not IS_BIGSTONE
 	// driver
-	HAL_SPI_DeInit(&hspi1);
-	HAL_SPI_Init(&hspi1);
-
-
-	HAL_Delay(10);
-
-	l6470_driver.init(1);
-	HAL_Delay(1);
-	l6470_driver.voltage_acc (0xDF);
-	HAL_Delay(1);
-	l6470_driver.voltage_dec (0xDF);
-	HAL_Delay(1);
-	l6470_driver.voltage_run (0xDF);
-	HAL_Delay(1);
-	l6470_driver.voltage_hold(0x30);
-
-	HAL_Delay(1000);
-
-	l6470_driver.Goto(5000);
-	HAL_Delay(5000);
-
-	l6470_driver.Run(1000, false);
-	HAL_Delay(1000);
-
-	l6470_driver.SoftStop();
-	HAL_Delay(1000);
-
-	l6470_driver.Goto(0);
-#else
+//	HAL_SPI_DeInit(&hspi1);
+//	HAL_SPI_Init(&hspi1);
+//
+//
+//	HAL_Delay(10);
+//
+//	l6470_driver.init(1);
+//	HAL_Delay(1);
+//	l6470_driver.voltage_acc (0xDF);
+//	HAL_Delay(1);
+//	l6470_driver.voltage_dec (0xDF);
+//	HAL_Delay(1);
+//	l6470_driver.voltage_run (0xDF);
+//	HAL_Delay(1);
+//	l6470_driver.voltage_hold(0x30);
+//
+//	HAL_Delay(1000);
+//
+//	l6470_driver.Goto(5000);
+//	HAL_Delay(5000);
+//
+//	l6470_driver.Run(1000, false);
+//	HAL_Delay(1000);
+//
+//	l6470_driver.SoftStop();
+//	HAL_Delay(1000);
+//
+//	l6470_driver.Goto(0);
 	// bigstone
-	l6470_driver_big.begin();
-	l6470_driver_big.SetHoldKVAL(0xff);
+	l6470_driver.begin();
+	l6470_driver.SetHoldKVAL(0xff);
 
-	l6470_driver_big.SoftReset();
+	l6470_driver.SoftReset();
 
-	l6470_driver_big.SetParam(ADR_ACC,2,0x40);
-	l6470_driver_big.SetParam(ADR_DEC,2,0x40);
-	l6470_driver_big.SetParam(ADR_MAX_SPEED,2,0x030);
-	l6470_driver_big.SetParam(ADR_MIN_SPEED,2,0x00);
-	l6470_driver_big.SetParam(ADR_FS_SPD,2,0x3ff);
-	l6470_driver_big.SetParam(ADR_KVAL_HOLD,1,0x50);
-	l6470_driver_big.SetParam(ADR_KVAL_RUN,1,0x50);
-	l6470_driver_big.SetParam(ADR_KVAL_ACC,1,0x50);
-	l6470_driver_big.SetParam(ADR_KVAL_DEC,1,0xf50);
-	l6470_driver_big.SetParam(ADR_STEP_MODE,1,0x07);
+	l6470_driver.SetParam(ADR_ACC,2,0x40);
+	l6470_driver.SetParam(ADR_DEC,2,0x40);
+	l6470_driver.SetParam(ADR_MAX_SPEED,2,0x030);
+	l6470_driver.SetParam(ADR_MIN_SPEED,2,0x00);
+	l6470_driver.SetParam(ADR_FS_SPD,2,0x3ff);
+	l6470_driver.SetParam(ADR_KVAL_HOLD,1,0x50);
+	l6470_driver.SetParam(ADR_KVAL_RUN,1,0x50);
+	l6470_driver.SetParam(ADR_KVAL_ACC,1,0x50);
+	l6470_driver.SetParam(ADR_KVAL_DEC,1,0xf50);
+	l6470_driver.SetParam(ADR_STEP_MODE,1,0x07);
 
-	l6470_driver_big.run(0x030000,CW);
-#endif
+	l6470_driver.run(0x030000,CW);
 }
 
 void loop(void){
